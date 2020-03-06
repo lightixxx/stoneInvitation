c6d8fbc527cad977ccf1268dd7bf1943fd1ea28e
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2020-03-06 16:00:37 +0900

    lightix
    background-attachment moible

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 	$('.slick-items').slick({
 		autoplay: true,
-		dots: true,
+		dots: false,
 		speed: 700 /* 이미지가 슬라이딩시 걸리는 시간 */ ,
 		infinite: true,
 		autoplaySpeed: 3000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */ ,
