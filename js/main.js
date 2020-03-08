1854a7e0d0b2be4ae78f8e1c0f0bfa7f0ff1bec3
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2020-03-08 17:53:45 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 	$('.slick-items').slick({
 		autoplay: true,
-		dots: false,
+		dots: true,
 		speed: 700 /* 이미지가 슬라이딩시 걸리는 시간 */ ,
 		infinite: true,
 		autoplaySpeed: 3000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */ ,
